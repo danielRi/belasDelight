fc252caa06db5aeaeea411ed5d4c634bc10b8cfb
Daniel Riegler <driegler@oknos.local>
2017-04-07 13:34:01 +0200

    Rechteck mit Keyboard steuern.

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,53 @@
+--[[
+    global variables
+]]
+
+RECTANGLE_MODE_FILL = "fill"
+KEYBOARD_DOWN_LEFT = "left"
+KEYBOARD_DOWN_RIGHT = "right"
+KEYBOARD_DOWN_DOWN = "down"
+KEYBOARD_DOWN_UP = "up"
+
+
+rectangleVelocity = 80;
+rectangleX = 0
+rectangleY = 0
+
+--[[
+    love callbacks
+]]
+
+-- called once at start
+function love.load()
+    
+end
+
+-- called each frame
+function love.update(deltaTime)
+    handleKeyboard(deltaTime)
+end
+
+-- called each frame
 function love.draw()
-    love.graphics.print("C.A.R.L.I", 400, 300)
+    love.graphics.rectangle(RECTANGLE_MODE_FILL, rectangleX, rectangleY, 30, 50)
+end
+
+
+--[[
+    input handler
+]]
+
+function handleKeyboard(deltaTime)
+    if love.keyboard.isDown(KEYBOARD_DOWN_LEFT) then
+        rectangleX = rectangleX - rectangleVelocity * deltaTime
+    end
+    if love.keyboard.isDown(KEYBOARD_DOWN_RIGHT) then
+        rectangleX = rectangleX + rectangleVelocity * deltaTime
+    end
+    if love.keyboard.isDown(KEYBOARD_DOWN_DOWN) then
+        rectangleY = rectangleY + rectangleVelocity * deltaTime
+    end
+    if love.keyboard.isDown(KEYBOARD_DOWN_UP) then
+        rectangleY = rectangleY - rectangleVelocity * deltaTime
+    end
 end
